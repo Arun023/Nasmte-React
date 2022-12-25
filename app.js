576a0cb612196f77e785143cf92bd88b3d19e50f
Arun Deep Singh Choudhary <arundev8153@gmail.com>
2022-12-25 10:55:26 +0530

    Inception

---
+++ app.js
@@ -0,0 +1,25 @@
+const root = document.getElementById("root");
+const ele = document.createElement("h1");
+ele.innerHTML = "Hello World";
+root.appendChild(ele);
+
+const heading = React.createElement("h1", { id: "title" }, "Heading1");
+const heading2 = React.createElement(
+  "h2",
+  {
+    id: "title",
+  },
+  "Heading 2"
+);
+
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading, heading2]
+);
+
+const root1 = ReactDOM.createRoot(document.getElementById("root1"));
+
+root1.render(container);
