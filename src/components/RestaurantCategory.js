36f01a4094d1b51f22daa7159be4f05bd6003d65
Arun023 <arundev8153@gmail.com>
2024-01-12 03:22:44 +0530

    Category accordian functionality implemenented

---
+++ src/components/RestaurantCategory.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { IoIosArrowDown } from "react-icons/io";
+const RestaurantCategory = (props) => {
+  const { itemCards, title, setCurrentIndex, currentIndex } = props;
+  return (
+    <div
+      className="my-4 cursor-pointer"
+      onClick={() => setCurrentIndex((prev) => (prev === title ? "" : title))}>
+      {/* Header or title */}
+      <div className="flex justify-between">
+        <div className="font-bold text-xl">
+          {title} ({itemCards?.length})
+        </div>
+        <IoIosArrowDown
+          className={`${
+            currentIndex === title ? "" : "rotate-180"
+          } duration-200`}
+          size={25}
+        />
+      </div>
+      {/* Body */}
+      {currentIndex === title && (
+        <div>
+          {itemCards.map((data) => {
+            return <div>{data?.card?.info?.name}</div>;
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default RestaurantCategory;
