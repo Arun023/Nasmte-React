cfbba4bea8d5d96260765d37e4d7f2428451b839
Arun023 <arundev8153@gmail.com>
2025-06-21 15:08:09 +0530

    Refactor index.js for consistency in import statements and formatting

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import "../index.css";
-import appRouter from "./Router";
-import setupLocatorUI from "@locator/runtime";
-if (process.env.NODE_ENV === "development") {
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import '../index.css';
+import appRouter from './Router';
+import setupLocatorUI from '@locator/runtime';
+if (process.env.NODE_ENV === 'development') {
   setupLocatorUI();
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<RouterProvider router={appRouter} />);
